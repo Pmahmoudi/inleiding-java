48f7b1f535eba72443e3ad38ba763f78d7852a74
inleidingJava <peyman_mah6@yahoo.com>
2018-01-30 10:53:00 +0100

    naam veranderd

--- src/h05/H05.java
+++ src/h05/H05.java
@@ -3,38 +3,80 @@ package h05;
 import java.applet.Applet;
 import java.awt.*;
 
-public class H05 extends Applet{
-
-    //declaratie
-    Color opvulkleur;
-    Color lijnkleur;
-    int breedte;
-    int hoogte;
-
+public class H05 extends Applet {
 
     @Override
     public void init() {
-        opvulkleur = Color.magenta;
-        lijnkleur = Color.black;
-        breedte = 200;
-        hoogte = 100;
+        super.init();
+
     }
 
 
     @Override
     public void paint(Graphics g) {
-        //teken rechthoek
-        g.drawRect(100,100,breedte, hoogte);
-        // teken afgronde rechthoek
-        g.drawRoundRect(100,225, breedte, hoogte, 30,30);
-        // teken gevulde rechthoek
-        g.setColor(opvulkleur);
-        g.fillRect(315,100,breedte,hoogte);
-        //teken ovaal
-        g.setColor(lijnkleur);
-        g.drawOval(315,100,breedte,hoogte);
-        //teken gevulde ovaal
-        g.setColor(opvulkleur);
-        g.fillOval(315,225,breedte,hoogte)
+        super.paint(g);
+
+
+        //line
+        g.drawLine(50,150,270,150);
+        g.drawLine(50,10,50,150);
+
+
+        //graphics
+        g.drawRect(70,50,20,100);
+        g.drawRect(130,70,20,80);
+        g.drawRect(190,110,20,40);
+
+
+
+         // variable jeroen
+        int jeroengewicht = 100;
+        int jeroenlengte = 50;
+
+        //variable hans
+        int hansgewicht = 80;
+        int hanslengte = 70;
+
+
+        //varibale valerie
+        int valeriegewicht = 40 ;
+        int valerielengte = 110;
+
+
+
+        //Color
+        g.fillRect(70,jeroenlengte,20, jeroengewicht);
+        //Hans
+        g.setColor(Color.pink);
+        g.fillRect(130,hanslengte,20,hansgewicht);
+        //valerie
+        g.setColor(Color.YELLOW);
+        g.fillRect(190,valerielengte,20,valeriegewicht);
+
+        g.setColor(Color.BLUE);
+        g.drawString("Hans",125,162);
+        g.drawString("Jeroen",60,162);
+        g.drawString("Valerie",182,162);
+
+
+        g.setColor(Color.BLACK);
+        g.drawLine(50,50,270,50);
+        g.drawLine(50,70,270,70);
+        g.drawLine(50,90,270,90);
+        g.drawLine(50,110,270,110);
+        g.drawLine(50,130,270,130);
+
+
+        g.setColor(Color.BLACK);
+        g.drawString("20KG",10,130);
+        g.drawString("40KG",10,110);
+        g.drawString("60KG",10,90);
+        g.drawString("80KG",10,70);
+        g.drawString("100KG",10,50);
+
+
+
     }
 }
+
+
